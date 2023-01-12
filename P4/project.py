ff6ffacef5be7a1be92d951753ea2ec443e211f7
Mratashnejad <mratashnejad@gmail.com>
2023-01-12 14:20:47 +0400

    codding

---
+++ P4/project.py
@@ -0,0 +1,25 @@
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+# یک عدد پالیندرومیک به هر دو صورت یکسان خوانده می شود. بزرگترین پالیندروم ساخته شده از حاصل ضرب دو عدد 2 رقمی 9009 = 91 × 99 است.
+
+# بزرگترین پالیندروم ساخته شده از حاصل ضرب دو عدد 3 رقمی را بیابید.
+
+num1 =0
+num2 =0
+def big_num(total):
+    
+    if bignumber > total: 
+            bignumber == total
+        return total 
+    else :
+        return num2
+
+def palindromicnum(num1 , num2):
+    total = 0
+    total = num1 * num2 
+
+            
+
+
