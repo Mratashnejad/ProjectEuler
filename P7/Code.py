cf3396ee7a0d73e848830567bef85f6cfb349922
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2023-02-12 14:22:39 +0400

    Update Code.py

--- P7/Code.py
+++ P7/Code.py
@@ -3,5 +3,20 @@
 
 def prime(x):
   primes = [2]
-  i = 3
+  y = 3
+  while len(primes < x):
+    for i in primes:
+      if i > int(y ** 0.5) + 1:
+        primes.append(y)
+        break
+      if y % i == 0:
+        break
+     y += 2
+   return primes[-1]
+
+
+
+print(prime(10001))
+  
+    
   
